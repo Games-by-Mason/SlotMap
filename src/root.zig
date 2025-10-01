03829dd6281c0f8197a4139fe69e1f2c8bd9180b
Mason Remaley <mason@anthropicstudios.com>
2025-10-01 15:00:02 -0700

    Less verbose handle formatting
    The double 0x created too much visual noise when looking over lists of handles

--- src/root.zig
+++ src/root.zig
@@ -48,7 +48,7 @@ pub fn SlotMap(Val: type, key_options: KeyOptions) type {
                     if (self == .invalid) {
                         try writer.writeAll(".invalid");
                     } else {
-                        try writer.print("0x{x}", .{@intFromEnum(self)});
+                        try writer.print("{x}", .{@intFromEnum(self)});
                     }
                 }
             };
@@ -101,7 +101,7 @@ pub fn SlotMap(Val: type, key_options: KeyOptions) type {
 
             pub fn format(self: @This(), writer: *std.Io.Writer) std.Io.Writer.Error!void {
                 assert(self.generation != .invalid);
-                try writer.print("0x{x}:{f}", .{ self.index, self.generation });
+                try writer.print("{x}:{f}", .{ self.index, self.generation });
             }
 
             pub fn eql(self: @This(), other: @This()) bool {
@@ -402,11 +402,11 @@ test "recycle key" {
 // Basically just making sure it compiles
 test "format key" {
     const Key = SlotMap(void, .{}).Key;
-    try std.testing.expectFmt("0xa:0xb", "{f}", .{Key{
+    try std.testing.expectFmt("a:b", "{f}", .{Key{
         .index = 10,
         .generation = @enumFromInt(11),
     }});
-    try std.testing.expectFmt("0xa:0xb", "{f}", .{(Key{
+    try std.testing.expectFmt("a:b", "{f}", .{(Key{
         .index = 10,
         .generation = @enumFromInt(11),
     }).toOptional()});
